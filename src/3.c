75ce2c632cc154161a95bb6662605eaabf02be4d
clarkjenniferc48842 <clark_jenniferc48842@gmx.com>
2025-03-05 14:32:54 +0000

    Add 3.c

---
+++ src/3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int x = 5;
+  int y = 6;
+  printf("The sum of %d and %d is %d\n", x, y, add(x, y));
+  return 0;
+}
+
+int add(int a, int b) {
+  return a + b;
+}
